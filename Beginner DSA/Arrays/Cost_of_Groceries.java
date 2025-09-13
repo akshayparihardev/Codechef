c6c881ff83d036a654d6b450f432ebc1f72baeb8
Akshay Parihar <akshayparihar9310@gmail.com>
2025-09-13 10:42:19 +0530

    Cost of Groceries

--- Beginner DSA/Arrays/Cost_of_Groceries.java
+++ Beginner DSA/Arrays/Cost_of_Groceries.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Cost_of_Groceries {
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
