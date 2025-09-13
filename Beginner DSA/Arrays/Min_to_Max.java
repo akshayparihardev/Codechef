23ec90b31af0e65841ae061e3fb8658844a493a8
Akshay Parihar <akshayparihar9310@gmail.com>
2025-09-13 10:42:58 +0530

    Min To Max

---
+++ Beginner DSA/Arrays/Min_to_Max.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Min_to_Max {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t > 0) {
+            int n = scanner.nextInt();
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = scanner.nextInt();
+            }
+
+            int min = a[0];
+            
+            for (int i = 0 ; i < n ; i++){
+                if (a[i] < min){
+                    min = a[i];
+                } 
+            }
+            
+            int count = 0;
+            
+            for (int i = 0 ; i < n ; i++){
+                if (a[i] > min){
+                    count++;
+                } 
+            }
+            
+            System.out.println(count);
+            t--;
+        }
+        scanner.close();
+    }
+}
