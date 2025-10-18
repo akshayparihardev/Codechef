eb29c1bab5a595287a6092f7fc9a92b1a54fdc18
Akshay Parihar <akshayparihar9310@gmail.com>
2025-10-18 19:49:31 +0530

    Check Sorting in Rotation

---
+++ Beginner DSA/Arrays/Sorted_Rotation.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Sorted_Rotation {
+
+    public static boolean check(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        boolean result = check(nums);
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
